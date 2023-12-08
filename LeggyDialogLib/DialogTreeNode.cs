e3c49319cd73b107fc7b1006c939dc95e8ccb62b
leigh966 <leigh966@hotmail.com>
2023-12-08 11:17:27 +0000

    Added LeggyTreeLib as a project dependency and made DialogNode a child class of TreeNode

--- LeggyDialogLib/DialogTreeNode.cs
+++ LeggyDialogLib/DialogTreeNode.cs
@@ -1,7 +1,18 @@
-﻿namespace LeggyDialogLib
+﻿using LeggyTreeLib;
+namespace LeggyDialogLib
 {
-    public class DialogTreeNode
+    public class DialogTreeNode :  TreeNode<string>
     {
-
+        private string _response;
+        public string Response { get { return _response; } }
+        private bool said = false;
+        public DialogTreeNode(string text, string response) : base(text) 
+        {
+            _response = response;
+        }
+        public DialogTreeNode(string text, string response, DialogTreeNode parent) : base(text, )
+        {
+            _response= response;
+        }
     }
 }
