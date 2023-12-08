746ff08dc7839306787fcf0b66ee3a819c22b9e7
leigh966 <leigh966@hotmail.com>
2023-12-08 16:34:09 +0000

    created a basic test to check that a dialogue tree can be saved without throwing an error - currently failing

--- TestLeggyDialogLib/TestDialogueTree.cs
+++ TestLeggyDialogLib/TestDialogueTree.cs
@@ -18,7 +18,19 @@ namespace TestLeggyDialogLib
             Assert.AreEqual(1, tree.Options.Length);
             tree.ConversationStart.Value.Said = true;
             Assert.AreEqual(2, tree.Options.Length);
+        }
 
+        [TestMethod]
+        public void TestSaveBasic()
+        {
+            DialogueTree tree = new DialogueTree("Hello", "Hi there!");
+            tree.ConversationStart.AddChild(new DialogueOption("How are you?", "I'm good thanks!"));
+            var node1 = tree.ConversationStart.Children[0];
+            node1.AddChild(new DialogueOption("Are you sure?", "Yep"));
+            tree.ConversationStart.AddChild(new DialogueOption("Weater is good huh?", "Nope"));
+            var node2 = tree.ConversationStart.Children[1];
+            node2.AddChild(new DialogueOption("Why?", "It's raining"));
+            tree.Save("test.dtree");
         }
     }
 }
